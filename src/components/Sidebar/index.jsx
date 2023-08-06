5422eae5a819b52555c0ee15d85e1e9ddc3799e0
snowari <pes1149@naver.com>
2023-08-07 01:06:15 +0900

    feat: 헤더 사이드 메뉴

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { ReactComponent as CancleIcon } from "../../assets/cancle.svg";
+
+const Wrapper = styled.div`
+  width: 304px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const CancelStyledIcon = styled(CancleIcon)`
+  margin-left: 258px;
+  margin-bottom: 70px;
+  margin-top: 44px;
+`;
+
+const ButtonWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Button = styled.button`
+  color: var(--black, #32363a);
+  font-family: PyeongChang Peace;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: normal;
+  width: 304px;
+  height: 60px;
+  align-items: center;
+  border-width: 0;
+  background-color: transparent;
+  border-width: 0;
+  border-bottom: 1px solid var(--gray-3, #d0d5e1);
+`;
+
+const Description = styled.p`
+  color: var(--black, #32363a);
+  font-family: PyeongChang;
+  font-size: 16px;
+  font-weight: 400;
+  margin-left: 30px;
+`;
+
+const Sidebar = ({ onClose }) => {
+  return (
+    <Wrapper>
+      <CancelStyledIcon onClick={onClose} />
+      <ButtonWrapper>
+        <Button>로그아웃</Button>
+        <Button>내 페이지로 가기</Button>
+      </ButtonWrapper>
+      <Description>문의 : ???</Description>
+    </Wrapper>
+  );
+};
+
+export default Sidebar;
--- src/pages/CardListPage/components/Header.jsx
+++ src/pages/CardListPage/components/Header.jsx
@@ -4,71 +4,113 @@ import { ReactComponent as HelperIcon } from "../assets/helper.svg";
 import { ReactComponent as MenuIcon } from "../assets/menu.svg";
 import { ReactComponent as BackIcon } from "../assets/back.svg";
 
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router";
+import Sidebar from "../../../components/Sidebar";
 
 const HeaderWrapper = styled.div`
-    z-index: 1000;
+  z-index: 1000;
 
-    position: fixed;
-    top: 0;
+  position: fixed;
+  top: 0;
 
-    width: 100vw;
-    height: 80px;
+  width: 100vw;
+  height: 80px;
 
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
 
-    backdrop-filter: blur(6px);
-    -webkit-backdrop-filter: blur(6px);
+  backdrop-filter: blur(6px);
+  -webkit-backdrop-filter: blur(6px);
 `;
 
 const HelperStyledIcon = styled(HelperIcon)`
-    margin-right: 16px;
-    margin-top: 44px;
+  margin-right: 16px;
+  margin-top: 44px;
 `;
 
 const MenuStyledIcon = styled(MenuIcon)`
-    margin-right: 20px;
-    margin-top: 44px;
+  margin-right: 20px;
+  margin-top: 44px;
+`;
+
+const MenuButton = styled.button`
+  background-color: transparent;
+  border-width: 0;
 `;
 
 const BackStyledIcon = styled(BackIcon)`
-    margin-left: 11px;
-    margin-top: 44px;
+  margin-left: 11px;
+  margin-top: 44px;
 
-    display: ${(props) => props.$display ? "block" : "none"};
+  display: ${(props) => (props.$display ? "block" : "none")};
 `;
 
 const Blank = styled.div`
-    flex: 1;
+  flex: 1;
+`;
+
+const SideMenuWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 100vh;
+  background-color: rgba(220, 233, 245, 1);
+  overflow-x: hidden;
+  transition: 0.5s;
+  z-index: 2000;
+
+  // open 상태에 따라 너비 변경
+  width: ${(props) => (props.open ? "304px" : "0")};
 `;
 
 /**
- * 
- * @param {{backactivate: boolean, backurl: string}} param0 
- * @returns 
+ *
+ * @param {{backactivate: boolean, backurl: string}} param0
+ * @returns
  */
 export default function Header({ backactivate, backurl }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <HeaderWrapper>
-                <BackStyledIcon $display={backactivate} onClick={() => navigate(backurl)} />
-                <Blank />
-                <div>
-                    <HelperStyledIcon />
-                    <MenuStyledIcon />
-                </div>
-            </HeaderWrapper>
-        </>
-    );
+  const handleMenuButton = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleCloseSideBar = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <HeaderWrapper>
+        <BackStyledIcon
+          $display={backactivate}
+          onClick={() => navigate(backurl)}
+        />
+        <Blank />
+        <div>
+          <MenuButton>
+            <HelperStyledIcon />
+          </MenuButton>
+          <MenuButton onClick={handleMenuButton}>
+            <MenuStyledIcon />
+          </MenuButton>
+        </div>
+      </HeaderWrapper>
+      <SideMenuWrapper open={open}>
+        <Sidebar onClose={handleCloseSideBar} />
+      </SideMenuWrapper>
+    </>
+  );
 }
 
 Header.propTypes = {
-    backactivate: PropTypes.bool,
-    backurl: PropTypes.string,
-}
+  backactivate: PropTypes.bool,
+  backurl: PropTypes.string,
+  onClick: PropTypes.bool,
+};
